7e8fd06fa4b365d2f37e1559e96edeb8651668fe
Younes <younes.bns@outlook.com>
2023-05-07 21:39:50 -0400

    I made 2 features with js one for the new page sign up and the second for the modal window

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// const modal = document.querySelector(`.modal`);
+// const popUp = document.querySelector(`.modal-absolute`);
+// const closeBtn = document.querySelector(`.close-modal`);
+
+// popUp.style.backgroundColor = "red";
+
+// modal.addEventListener(`click`, function () {
+//   popUp.style.display = `block`;
+// });
+// closeBtn.addEventListener(`click`, function () {
+//   popUp.style.display = `none`;
+// });
+
+const name = "John";
+console.log(`Hello, ${name}!`);
