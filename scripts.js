15d2c1ddf37efda8b03c42a5b889767714e17593
Younes <younes.bns@outlook.com>
2023-05-22 15:37:05 -0400

    need to work more in the cart shopping modal

--- scripts.js
+++ scripts.js
@@ -1,15 +1,63 @@
-// const modal = document.querySelector(`.modal`);
-// const popUp = document.querySelector(`.modal-absolute`);
-// const closeBtn = document.querySelector(`.close-modal`);
+const modal = document.querySelector(".modal");
+const BtnCloseModal = document.querySelector(".close-modal");
 
-// popUp.style.backgroundColor = "red";
+BtnCloseModal.addEventListener(`click`, function () {
+  modal.style.display = `none`;
+});
 
-// modal.addEventListener(`click`, function () {
-//   popUp.style.display = `block`;
-// });
-// closeBtn.addEventListener(`click`, function () {
-//   popUp.style.display = `none`;
-// });
+// js for cart section
 
-const name = "John";
-console.log(`Hello, ${name}!`);
+const cart = document.querySelector(`#cart`);
+const cartContainer = document.querySelector(`.cart-container`);
+const itemsContainer = document.querySelector(`.items-container`);
+// open cart modal
+
+cart.addEventListener("click", function () {
+  if (cartContainer.style.display === "none") {
+    cartContainer.style.display = "flex";
+  } else {
+    cartContainer.style.display = "none";
+  }
+});
+// End for cart modal
+
+// close cart modal with x
+
+const closeBtnCart = document.querySelector(`#close-btn-cart`);
+closeBtnCart.addEventListener(`click`, function () {
+  cartContainer.style.display = "none";
+});
+
+cartContainer.addEventListener(`click`, (e) => {
+  if (e.target.classList.contains(`cart-container`)) {
+    cartContainer.style.display = `none`;
+  }
+});
+// End clor cart modal within the class
+
+// add items to cart
+
+const addTheItem = document.getElementsByClassName("add-the-item");
+const productCart = document.getElementsByClassName("product-cart");
+for (let i = 0; i < addTheItem.length; i++) {
+  const addTheItemButton = addTheItem[i];
+  addTheItemButton.addEventListener(`click`, addToCartClicked);
+}
+
+function addToCartClicked(event) {
+  addTheItemButton = event.target;
+  let cartItems = addTheItemButton.parentElement;
+  let price = cartItems.getElementsByClassName("product-price")[0].textContent;
+  let imageSrc = cartItems.getElementsByClassName("product-image")[0].src;
+  addProductsToCart(price, imageSrc);
+  updateCartPrice();
+  cartContainer.style.display = "none"; // i have to check this one
+  itemsContainer.style.display = "block";
+}
+
+function addProductsToCart(price, imageSrc) {
+  let productCart = document.createElement(`div`);
+  productCart.classList.add("product-cart");
+
+  let productsCart = document.getElementsByClassName("product-cart")[0];
+}
